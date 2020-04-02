3f491a416a1fdfc137ad609ef048c792966a060a
usamabinshahid97 <i150238@nu.edu.pk>
2020-04-03 00:52:32 +0500

    Code separation and links functioning

---
+++ dashboard/src/Components/Trajectory/trajectory.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './trajectory.scss'
+
+const Trajectory: React.FC<any> = () => {
+  return (
+      <div className="trajectory">
+           
+      </div>
+  );
+};
+export default Trajectory;
---
+++ dashboard/src/Components/Zone/zone.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './zone.scss'
+
+const Zone: React.FC<any> = () => {
+  return (
+      <div className="zone">
+           
+      </div>
+  );
+};
+export default Zone;
