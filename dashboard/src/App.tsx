d86cad4a8c42c6758699bc805e8f041a66314a9f
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-04-03 14:14:38 +0500

    Fix map bug

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Navbar from './Components/navbar/navbar'
-import MapView from './Components/MapView/mapview'
-import Trajectory from './Components/Trajectory/trajectory'
-import News from './Components/News/news'
-import Zone from './Components/Zone/zone'
+import Navbar from "./Components/navbar/navbar";
+import MapView from "./Components/MapView/mapview";
+import Trajectory from "./Components/Trajectory/trajectory";
+import News from "./Components/News/news";
+import Zone from "./Components/Zone/zone";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
@@ -16,13 +16,13 @@ const App: React.FC<any> = () => {
       <div className="App">
         <Navbar></Navbar>
         <Switch>
-          <Route exact path='/' component = { MapView }/>
-          <Route exact path='/trajectory' component = { Trajectory }/>
-          <Route exact path='/trajectory' component = { News }/>
-          <Route exact path='/trajectory' component = { Zone }/>
+          <Route exact path="/" component={MapView} />
+          <Route exact path="/trajectory" component={Trajectory} />
+          <Route exact path="/trajectory" component={News} />
+          <Route exact path="/trajectory" component={Zone} />
         </Switch>
       </div>
-  </BrowserRouter>
+    </BrowserRouter>
   );
 };
 export default App;
