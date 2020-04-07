7d9345af590458df53174d9adf9d0c4e8692b2a3
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-04-08 03:03:04 +0500

    Fix formatting

--- dashboard/src/Components/admin/Admin.tsx
+++ dashboard/src/Components/admin/Admin.tsx
@@ -1,37 +1,36 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import "./Admin.scss";
 
-import { CSVReader } from 'react-papaparse'
+import { CSVReader } from "react-papaparse";
 import { Button, Alert } from "react-bootstrap";
-
 
 const Admin: React.FC<any> = () => {
   const buttonRef: any = React.createRef();
 
   const [is_uploaded, setIsUploaded] = useState(false);
   const handleOnFileLoad = (data: any) => {
-    setIsUploaded(true)
-    console.log(data)
-  }
- 
+    setIsUploaded(true);
+    console.log(data);
+  };
+
   const handleOnError = (err: any, file: any, inputElem: any, reason: any) => {
-    console.log(err)
-  }
+    console.log(err);
+  };
   const alert = is_uploaded ? (
     <div>
       <Alert variant="success">
         <Alert.Heading>File uploaded successfully!</Alert.Heading>
       </Alert>
     </div>
-  ) : null
- 
+  ) : null;
+
   const handleOpenDialog = (e: any) => {
-    // Note that the ref is set async, so it might be null at some point 
+    // Note that the ref is set async, so it might be null at some point
     if (buttonRef.current) {
-      buttonRef.current.open(e)
+      buttonRef.current.open(e);
     }
-  }
+  };
 
   return (
     <div className="admin">
@@ -43,12 +42,14 @@ const Admin: React.FC<any> = () => {
         noClick
         noDrag
       >
-        {({file}: any) => (
-          <aside style={{display: 'flex', flexDirection: 'row', marginBottom: 10}}>
+        {({ file }: any) => (
+          <aside
+            style={{ display: "flex", flexDirection: "row", marginBottom: 10 }}
+          >
             <div className="browse-button">
-            <Button variant="primary" onClick={handleOpenDialog}>
-              Browe file
-            </Button>
+              <Button variant="primary" onClick={handleOpenDialog}>
+                Browe file
+              </Button>
             </div>
           </aside>
         )}
