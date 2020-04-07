727812e240efccab45c16f3284087e7f4c335e21
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-04-07 17:46:33 +0500

    Added mapbox access token

--- dashboard/src/shared/environment/environment.ts
+++ dashboard/src/shared/environment/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   googleMapsApiKey: "AIzaSyDFb6-9lcXRaHDEkjEHGdVDC7FpKrxNjss",
-  ipStackApiKey: "9a86dd48f868267ed9eb9ab8e52129f5"
+  ipStackApiKey: "9a86dd48f868267ed9eb9ab8e52129f5",
+  mapBoxAccessToken:
+    "pk.eyJ1IjoiYWhtYWR5YXJraGFuNTU1IiwiYSI6ImNrOHB1MmRxcDAxcWkzZHBqNXBqbHNmbnAifQ.9Dhj8IkFTx3eS9o0tL_29g"
 };
