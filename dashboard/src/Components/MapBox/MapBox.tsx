5240e77a08ec9707a76278da0fb3d8c5047531e1
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-04-14 01:00:27 +0500

    Attemped typescript error fix

--- dashboard/src/Components/MapBox/MapBox.tsx
+++ dashboard/src/Components/MapBox/MapBox.tsx
@@ -10,6 +10,7 @@ import * as PolySindh from "../../geojson/sindh";
 import * as PolyAJK from "../../geojson/ajk";
 import * as PolyBalochistan from "../../geojson/balochistan";
 import * as PolyFata from "../../geojson/fata";
+import * as xyz from "./xyz";
 import "./MapBox.scss";
 
 const provinces: Polygon[] = [
@@ -25,6 +26,7 @@ const provinces: Polygon[] = [
 const MapBoxComponent: React.FC<any> = () => {
   const [map, setMap] = useState<mapboxgl.Map>();
   const [mapReady, setMapReady] = useState<boolean>(false);
+  const [context, setContext] = useState<any>();
   var myref: any = React.createRef();
   var size = 200;
 
@@ -33,72 +35,55 @@ const MapBoxComponent: React.FC<any> = () => {
   }, []);
 
   useEffect(() => {
-    drawProvincePolygons();
+    // drawProvincePolygons();
   }, [map]);
 
   var pulsingDot = {
     width: size,
     height: size,
     data: new Uint8Array(size * size * 4),
-     
+
     // get rendering context for the map canvas when layer is added to the map
     onAdd: function() {
-      var canvas = document.createElement('canvas');
+      var canvas = document.createElement("canvas");
       canvas.width = this.width;
       canvas.height = this.height;
-      myref = canvas.getContext('2d');
+      setContext(canvas.getContext("2d"));
     },
-     
+
     // called once before every frame where the icon will be used
     render: function() {
-    var duration = 1000;
-    var t = (performance.now() % duration) / duration;
-     
-    var radius = (size / 2) * 0.3;
-    var outerRadius = (size / 2) * 0.7 * t + radius;
-    var context = myref;
-     
-    // draw outer circle
-    context.clearRect(0, 0, this.width, this.height);
-    context.beginPath();
-    context.arc(
-    this.width / 2,
-    this.height / 2,
-    outerRadius,
-    0,
-    Math.PI * 2
-    );
-    context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
-    context.fill();
-     
-    // draw inner circle
-    context.beginPath();
-    context.arc(
-    this.width / 2,
-    this.height / 2,
-    radius,
-    0,
-    Math.PI * 2
-    );
-    context.fillStyle = 'rgba(255, 100, 100, 1)';
-    context.strokeStyle = 'white';
-    context.lineWidth = 2 + 4 * (1 - t);
-    context.fill();
-    context.stroke();
-     
-    // update this image's data with data from the canvas
-    this.data = context.getImageData(
-    0,
-    0,
-    this.width,
-    this.height
-    ).data;
-     
-    // continuously repaint the map, resulting in the smooth animation of the dot
-    map!.triggerRepaint();
-     
-    // return `true` to let the map know that the image was updated
-    return true;
+      var duration = 1000;
+      var t = (performance.now() % duration) / duration;
+
+      var radius = (size / 2) * 0.3;
+      var outerRadius = (size / 2) * 0.7 * t + radius;
+      var context: any = context;
+
+      // draw outer circle
+      context.clearRect(0, 0, this.width, this.height);
+      context.beginPath();
+      context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2);
+      context.fillStyle = "rgba(255, 200, 200," + (1 - t) + ")";
+      context.fill();
+
+      // draw inner circle
+      context.beginPath();
+      context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
+      context.fillStyle = "rgba(255, 100, 100, 1)";
+      context.strokeStyle = "white";
+      context.lineWidth = 2 + 4 * (1 - t);
+      context.fill();
+      context.stroke();
+
+      // update this image's data with data from the canvas
+      this.data = context.getImageData(0, 0, this.width, this.height).data;
+
+      // continuously repaint the map, resulting in the smooth animation of the dot
+      map!.triggerRepaint();
+
+      // return `true` to let the map know that the image was updated
+      return true;
     }
   };
 
@@ -106,36 +91,25 @@ const MapBoxComponent: React.FC<any> = () => {
     let map = new mapboxgl.Map({
       accessToken: environment.mapBoxAccessToken,
       container: "map-gl-container",
-      center: [69.3451, 30.3753],
+      center: [0, 0],
       zoom: 5,
       style: MapStyles.Dark,
       boxZoom: true
     });
     map.on("load", () => {
-      map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
-      map.addSource('points', {
-        'type': 'geojson',
-        'data': {
-          'type': 'FeatureCollection',
-          'features': [
-            {
-              'type': 'Feature',
-              'geometry': {
-                'type': 'Point',
-                'coordinates': [0, 0]
-              }
-            }
-          ]
-        }
-        });
+      map.addImage("pulsing-dot", pulsingDot, { pixelRatio: 2 });
+      map.addSource("points", {
+        type: "geojson",
+        data: JSON.stringify(xyz.default) as any
+      });
       map.addLayer({
-        'id': 'points',
-        'type': 'symbol',
-        'source': 'points',
-        'layout': {
-        'icon-image': 'pulsing-dot'
+        id: "points",
+        type: "symbol",
+        source: "points",
+        layout: {
+          "icon-image": "pulsing-dot"
         }
-        });
+      });
       setMap(map);
       setMapReady(true);
       map.addControl(
---
+++ dashboard/src/Components/MapBox/xyz.ts
@@ -0,0 +1,12 @@
+export default {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [0, 0]
+      }
+    }
+  ]
+};
