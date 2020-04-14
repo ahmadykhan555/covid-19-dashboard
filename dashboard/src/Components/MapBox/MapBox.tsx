67969160fea0c00042d4617d5502bed32d7d1ea2
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-04-14 20:50:59 +0500

    Added sample pulsing dot, Usama to configure for all countries

--- dashboard/src/Components/MapBox/MapBox.tsx
+++ dashboard/src/Components/MapBox/MapBox.tsx
@@ -10,6 +10,7 @@ import * as PolySindh from "../../geojson/sindh";
 import * as PolyAJK from "../../geojson/ajk";
 import * as PolyBalochistan from "../../geojson/balochistan";
 import * as PolyFata from "../../geojson/fata";
+import * as Circle from "./circle";
 import "./MapBox.scss";
 
 interface MapComponentProps {
@@ -29,6 +30,7 @@ const provinces: Polygon[] = [
 const MapBoxComponent: React.FC<MapComponentProps> = ({ center }) => {
   const [map, setMap] = useState<mapboxgl.Map>();
   const [mapReady, setMapReady] = useState<boolean>(false);
+  // const [context, setContext] = useState<any>();
 
   useEffect(() => {
     initMap();
@@ -45,6 +47,7 @@ const MapBoxComponent: React.FC<MapComponentProps> = ({ center }) => {
 
   useEffect(() => {
     drawProvincePolygons();
+    drawCircles();
   }, [map]);
 
   const initMap = () => {
@@ -67,7 +70,6 @@ const MapBoxComponent: React.FC<MapComponentProps> = ({ center }) => {
           trackUserLocation: true
         })
       );
-
       map.addControl(new mapboxgl.NavigationControl());
     });
   };
@@ -92,6 +94,81 @@ const MapBoxComponent: React.FC<MapComponentProps> = ({ center }) => {
         paint: {
           "fill-color": `#${config.color}`,
           "fill-opacity": 0.5
+        }
+      });
+    }
+  };
+
+  const drawCircles = () => {
+    if (map) {
+      var context: any;
+      let size = 100;
+      var pulsingDot = {
+        width: size,
+        height: size,
+        data: new Uint8Array(size * size * 4),
+
+        // get rendering context for the map canvas when layer is added to the map
+        onAdd: function() {
+          var canvas = document.createElement("canvas");
+          canvas.width = this.width;
+          canvas.height = this.height;
+          context = canvas.getContext("2d");
+        },
+
+        // called once before every frame where the icon will be used
+        render: function() {
+          var duration = 1000;
+          var t = (performance.now() % duration) / duration;
+
+          var radius = (size / 2) * 0.3;
+          var outerRadius = (size / 2) * 0.7 * t + radius;
+
+          // draw outer circle
+          context.clearRect(0, 0, this.width, this.height);
+          context.beginPath();
+          context.arc(
+            this.width / 2,
+            this.height / 2,
+            outerRadius,
+            0,
+            Math.PI * 2
+          );
+          context.fillStyle = "rgba(255, 200, 200," + (1 - t) + ")";
+          context.fill();
+
+          // draw inner circle
+          context.beginPath();
+          context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
+          context.fillStyle = "rgba(255, 100, 100, 1)";
+          context.strokeStyle = "white";
+          context.lineWidth = 2 + 4 * (1 - t);
+          context.fill();
+          context.stroke();
+
+          // update this image's data with data from the canvas
+          this.data = context.getImageData(0, 0, this.width, this.height).data;
+
+          // continuously repaint the map, resulting in the smooth animation of the dot
+          if (map) {
+            map.triggerRepaint();
+          }
+
+          // return `true` to let the map know that the image was updated
+          return true;
+        }
+      };
+      map.addImage("pulsing-dot", pulsingDot, { pixelRatio: 2 });
+      map.addSource("points", {
+        type: "geojson",
+        data: Circle.default as any
+      });
+      map.addLayer({
+        id: "points",
+        type: "symbol",
+        source: "points",
+        layout: {
+          "icon-image": "pulsing-dot"
         }
       });
     }
