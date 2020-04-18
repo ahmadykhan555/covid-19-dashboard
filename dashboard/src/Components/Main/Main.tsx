b6933d538f06400cf7e642e407121dabdf2b16ca
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-04-18 20:58:54 +0500

    PIe chart concept WIP

--- dashboard/src/Components/Main/Main.tsx
+++ dashboard/src/Components/Main/Main.tsx
@@ -3,11 +3,13 @@ import "./Main.scss";
 import MapBoxComponent from "../MapBox/MapBox";
 import SwitcherComponent from "../../components/Switcher/Switcher";
 import * as mapboxgl from "mapbox-gl";
+import { ResponsivePie } from "@nivo/pie";
 
 import LocalStatsComponent from "../LocalStats/LocalStats";
 import GlobalStatsComponent from "../GlobalStats/GlobalStats";
 import SummaryTiles from "../SummaryTiles/SummaryTiles";
 import moment from "moment";
+import PieChart from "../Graphs/PieChart/PieChart";
 const MainLayoutComponent: React.FC<any> = () => {
   const [globalStats, setGlobalStats] = useState<boolean>(true);
   const [mapCenter, setMapCenter] = useState<mapboxgl.LngLat>(
@@ -45,7 +47,41 @@ const MainLayoutComponent: React.FC<any> = () => {
       </section>
       <div className="section-center">
         <section className="map-container">
-          <MapBoxComponent center={mapCenter} />
+          {/* <MapBoxComponent center={mapCenter} /> */}
+          <PieChart
+            data={[
+              {
+                id: "php",
+                label: "php",
+                value: 88,
+                color: "hsl(204, 70%, 50%)"
+              },
+              {
+                id: "go",
+                label: "go",
+                value: 35,
+                color: "hsl(354, 70%, 50%)"
+              },
+              {
+                id: "javascript",
+                label: "javascript",
+                value: 437,
+                color: "hsl(119, 70%, 50%)"
+              },
+              {
+                id: "rust",
+                label: "rust",
+                value: 214,
+                color: "hsl(155, 70%, 50%)"
+              },
+              {
+                id: "python",
+                label: "python",
+                value: 591,
+                color: "hsl(352, 70%, 50%)"
+              }
+            ]}
+          />
         </section>
       </div>
     </div>
