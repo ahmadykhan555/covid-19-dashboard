af7ea94180aaaee8102629758ddccdd03678a79c
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-04-25 04:07:15 +0500

    remove ipstack provider

--- dashboard/src/shared/ip-resolver/api.ts
+++
@@ -1,8 +0,0 @@
-import { environment } from "./../environment/environment";
-import axios from "axios";
-
-export const resolveLocationByIp = () => {
-  return axios.get(
-    `http://api.ipstack.com/check?access_key=${environment.ipStackApiKey}`
-  );
-};
