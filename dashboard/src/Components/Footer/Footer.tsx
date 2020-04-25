391885533e2fcfc2d15598f5f5210de0e88245f0
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-04-26 04:49:08 +0500

    Make year in footer dynamic

--- dashboard/src/Components/Footer/Footer.tsx
+++ dashboard/src/Components/Footer/Footer.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Footer.scss";
+import moment from "moment";
 
 const Footer: React.FC<any> = () => {
   return (
     <div className="footer">
       <div className="footer-inner">
-        <h4 className="copyright-info">© 2020 All rights reserved</h4>
+        <h4 className="copyright-info">
+          © {moment().year()} All rights reserved
+        </h4>
         <ul className="links">
           <li>About</li>
           <li>Legal</li>
