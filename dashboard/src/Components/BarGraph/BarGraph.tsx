5c387c133b72a17982a090923d29bb1be6f39755
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-04-26 05:11:14 +0500

    Fix formatting and remove dead code

--- dashboard/src/Components/BarGraph/BarGraph.tsx
+++ dashboard/src/Components/BarGraph/BarGraph.tsx
@@ -11,89 +11,57 @@ const BarGraphComponent: any = () => {
       style={{ height: "calc(100% - 30px)", width: "calc(100% - 20px)" }}
     >
       <ResponsiveBar
-        colors={{ scheme: 'nivo' }}
+        colors={{ scheme: "nivo" }}
         data={[
-            {
-                "month": "Jan",
-                "cases": 75,
-                "casesColor": "hsl(210, 70%, 50%)"
-              },
-              {
-                "month": "Feb",
-                "cases": 186,
-                "casesColor": "hsl(222, 70%, 50%)"
-              },
-              {
-                "month": "March",
-                "cases": 93,
-                "casesColor": "hsl(338, 70%, 50%)"
-              },
-              {
-                "month": "April",
-                "cases": 134,
-                "casesColor": "hsl(86, 70%, 50%)"
-              }
+          {
+            month: "Jan",
+            cases: 75,
+            casesColor: "hsl(210, 70%, 50%)"
+          },
+          {
+            month: "Feb",
+            cases: 186,
+            casesColor: "hsl(222, 70%, 50%)"
+          },
+          {
+            month: "March",
+            cases: 93,
+            casesColor: "hsl(338, 70%, 50%)"
+          },
+          {
+            month: "April",
+            cases: 134,
+            casesColor: "hsl(86, 70%, 50%)"
+          }
         ]}
-        padding = {0.7}
-        keys={[ 'cases' ]}
+        padding={0.7}
+        keys={["cases"]}
         indexBy="month"
         margin={{ top: 10, right: 10, bottom: 25, left: 60 }}
-        borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+        borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
         axisTop={null}
         axisRight={null}
         axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'country',
-            legendPosition: 'middle',
-            legendOffset: 32
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "country",
+          legendPosition: "middle",
+          legendOffset: 32
         }}
         axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'food',
-            legendPosition: 'middle',
-            legendOffset: -40
+          tickSize: 5,
+          tickPadding: 5,
+          tickRotation: 0,
+          legend: "food",
+          legendPosition: "middle",
+          legendOffset: -40
         }}
-        // defs={[
-        //     {
-        //         id: 'dots',
-        //         type: 'patternDots',
-        //         background: 'inherit',
-        //         color: '#38bcb2',
-        //         size: 4,
-        //         padding: 1,
-        //         stagger: true
-        //     },
-        //     {
-        //         id: 'lines',
-        //         type: 'patternLines',
-        //         background: 'inherit',
-        //         color: '#eed312',
-        //         rotation: -45,
-        //         lineWidth: 6,
-        //         spacing: 10
-        //     }
-        // ]}
-        // fill={[
-        //     {
-        //         match: {
-        //             id: 'fries'
-        //         },
-        //         id: 'dots'
-        //     },
-        //     {
-        //         match: {
-        //             id: 'sandwich'
-        //         },
-        //         id: 'lines'
-        //     }
-        // ]}
         labelSkipWidth={12}
         labelSkipHeight={12}
-        labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+        labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+        enableGridX={false}
+        enableGridY={false}
       />
     </div>
   );
