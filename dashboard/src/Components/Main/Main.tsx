d4277b87c5d940307733aca311336df0ecec5910
Ahmad Yar Khan <ahmadyarkhan555@gmail.com>
2020-06-23 23:15:31 +0500

    Cleanup main.tsx, modularize code

--- dashboard/src/Components/Main/Main.tsx
+++ dashboard/src/Components/Main/Main.tsx
@@ -13,81 +13,100 @@ const MainLayoutComponent: React.FC<PropsFromRedux> = ({
   menuExpanded,
   setMenuExpanded
 }) => {
-  return (
-    <div className="main-layout-component">
-      {menuExpanded && <SideMenu />}
-      {!menuExpanded && (
-        <section className="section-left">
-          <div className="header-sec-left">
-            <div className="app-banner">
-              {isMobile && (
-                <GiHamburgerMenu
-                  onClick={() => setMenuExpanded(!menuExpanded)}
-                  style={{
-                    position: "absolute",
-                    left: "1rem",
-                    fontSize: "1rem"
-                  }}
-                />
-              )}
-              <h1>COVID WORLD TRACKER</h1>
-            </div>
-            {!isMobile && (
-              <div
-                className="useful-links"
-                style={{ marginTop: "1rem", display: "flex" }}
-              >
-                <a
-                  className="link-donation"
-                  href="https://www.covidplasma.pk"
-                  target="_blank"
-                  style={{ fontSize: "0.875rem", color: "black" }}
-                >
-                  <span>Plasma Donation</span>
-                  <MdLocalHospital
-                    style={{ marginLeft: "0.5rem", fontSize: "1.25rem" }}
-                  />
-                </a>
-              </div>
+  const sideMenuMobile = () => {
+    return <SideMenu />;
+  };
+
+  const listView = () => {
+    return (
+      <>
+        <div className="header-sec-left">
+          <div className="app-banner">
+            {isMobile && (
+              <GiHamburgerMenu
+                onClick={() => setMenuExpanded(!menuExpanded)}
+                style={{
+                  position: "absolute",
+                  left: "1rem",
+                  fontSize: "1rem"
+                }}
+              />
             )}
-            <div className="stats-definitions">
-              <div className="def-cell def-cell--cases">
-                <div className="def-cell__rect"></div>
-                <h4 className="def-cell__label">Cases</h4>
-              </div>
-              <div className="def-cell def-cell--deaths">
-                <div className="def-cell__rect"></div>
-                <h4 className="def-cell__label">Deaths</h4>
-              </div>
-              <div className="def-cell def-cell--recovered">
-                <div className="def-cell__rect"></div>
-                <h4 className="def-cell__label">Recovered</h4>
-              </div>
-              <div className="def-cell def-cell--per-mil">
-                <div className="def-cell__rect"></div>
-                <h4 className="def-cell__label">Cases/Million</h4>
-              </div>
+            <h1>COVID WORLD TRACKER</h1>
+          </div>
+          {!isMobile && (
+            <div
+              className="useful-links"
+              style={{ marginTop: "1rem", display: "flex" }}
+            >
+              <a
+                className="link-donation"
+                href="https://www.covidplasma.pk"
+                target="_blank"
+                style={{ fontSize: "0.875rem", color: "black" }}
+              >
+                <span>Plasma Donation</span>
+                <MdLocalHospital
+                  style={{ marginLeft: "0.5rem", fontSize: "1.25rem" }}
+                />
+              </a>
+            </div>
+          )}
+          <div className="stats-definitions">
+            <div className="def-cell def-cell--cases">
+              <div className="def-cell__rect"></div>
+              <h4 className="def-cell__label">Cases</h4>
+            </div>
+            <div className="def-cell def-cell--deaths">
+              <div className="def-cell__rect"></div>
+              <h4 className="def-cell__label">Deaths</h4>
+            </div>
+            <div className="def-cell def-cell--recovered">
+              <div className="def-cell__rect"></div>
+              <h4 className="def-cell__label">Recovered</h4>
+            </div>
+            <div className="def-cell def-cell--per-mil">
+              <div className="def-cell__rect"></div>
+              <h4 className="def-cell__label">Cases/Million</h4>
             </div>
           </div>
-          {<AllCountriesStatsComponent />}
-        </section>
-      )}
+        </div>
+        {<AllCountriesStatsComponent />}
+      </>
+    );
+  };
+
+  const mapView = () => {
+    return (
+      <section className="map-container">
+        <MapBoxComponent />
+      </section>
+    );
+  };
+
+  const graphView = () => {
+    return (
+      <section
+        className={`graphs-overlay ${graphViewExpanded ? "expanded" : ""}`}
+      >
+        <GraphViewComponent />
+      </section>
+    );
+  };
+
+  return (
+    <div className="main-layout-component">
+      {menuExpanded && sideMenuMobile()}
+      <section className="section-left">{!menuExpanded && listView()}</section>
       <div className="section-right">
-        {!isMobile && (
-          <section className="map-container">
-            <MapBoxComponent />
-          </section>
-        )}
-        <section
-          className={`graphs-overlay ${graphViewExpanded ? "expanded" : ""}`}
-        >
-          <GraphViewComponent />
-        </section>
+        {!isMobile && mapView()}
+        {graphView()}
       </div>
     </div>
   );
 };
 
+// Connect to redux
 const mapStateToProps = (state: AppState) => {
   return {
     graphViewExpanded: state.graphViewExpanded,
